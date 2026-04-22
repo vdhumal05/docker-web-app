8177dcdc7e4c1b4d8ff7c2f3952cbe37add0c6d4
vdhumal05 <vaishnavidhumal150605@gmail..com>
2026-04-22 10:49:22 +0530

    Added task 3 terraform

---
+++ terraform-project/main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    local = {
+      source = "hashicorp/local"
+      version = "~> 2.4"
+    }
+  }
+}
+
+provider "local" {}
+
+resource "local_file" "example" {
+  filename = "hello.txt"
+  content  = "Hello, Terraform!"
+}
